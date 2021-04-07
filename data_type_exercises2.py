0800c912aa4abf9c2af5443d20ed9881c45f27f8
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-04-07 10:27:46 -0500

    Add answers

--- data_type_exercises2.py
+++ data_type_exercises2.py
@@ -1,28 +1,73 @@
-#                           DATA TYPES EXERCISES
-####  For each of the following code blocks, read the expression and predict what the result of evaluating it would be, 
-####then execute the expression in your Python REPL.
+"""
+                           DATA TYPES EXERCISES
 
-  
+
+For each of the following code blocks, read the expression and predict what the result of evaluating it would be, 
+then execute the expression in your Python REPL.
+"""
+# I will get an error because we are adding a string with a number  
 '1' + 2
+
+#  2
 6 % 4
+
+# int
 type(6 % 4)
+
+# string
 type(type(6 % 4))
+
+#  3 + 4 is 7
 '3 + 4 is ' + 3 + 4
+
+# False
 0 < 0
+
+# False
 'False' == False
+
+# False
 True == 'True'
+
+# False
 5 >= -5
+
+# True
 True or "42"
+
+# 1
 6 % 5
+
+# False
 5 < 4 and 1 == 1
+
+# False
 'codeup' == 'codeup' and 'codeup' == 'Codeup'
+
+# True
 4 >= 0 and 1 !== '1'
+
+# True
 6 % 3 == 0
+
+# True
 5 % 2 != 0
+
+# error
 [1] + 2
+
+# [1,2]
 [1] + [2]
+
+# error
 [1] * 2
+
+# [2]
 [1] * [2]
+
+#true
 [] + [] == []
+
+#
 {} + {}
 
