66f88d26c7b1dab6532110ed09f9c490b9b40c51
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-04-06 22:25:59 -0500

    Add more exercises

---
+++ data_type_exercises2.py
@@ -0,0 +1,28 @@
+#                           DATA TYPES EXERCISES
+####  For each of the following code blocks, read the expression and predict what the result of evaluating it would be, 
+####then execute the expression in your Python REPL.
+
+  
+'1' + 2
+6 % 4
+type(6 % 4)
+type(type(6 % 4))
+'3 + 4 is ' + 3 + 4
+0 < 0
+'False' == False
+True == 'True'
+5 >= -5
+True or "42"
+6 % 5
+5 < 4 and 1 == 1
+'codeup' == 'codeup' and 'codeup' == 'Codeup'
+4 >= 0 and 1 !== '1'
+6 % 3 == 0
+5 % 2 != 0
+[1] + 2
+[1] + [2]
+[1] * 2
+[1] * [2]
+[] + [] == []
+{} + {}
+
