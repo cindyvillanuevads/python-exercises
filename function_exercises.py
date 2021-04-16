0f22cb05791fb52beadcbd6549c8939b6331f173
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-04-15 23:58:05 -0500

    completed exercises

--- function_exercises.py
+++ function_exercises.py
@@ -12,12 +12,7 @@ def is_two(number):
 # def is_two(x):
 #     return x == 2 or x == "2"
 
-print ("Exercise 1")
-print (is_two(2))
-print( is_two(2.0)) 
-print( is_two("2")) 
-print( is_two(5)) 
-print( is_two("coffee"))
+
 
 # 2. Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
 
@@ -34,10 +29,6 @@ def is_vowel(letter):
 #         return result
 #     else:
 #         return False
-print("Exercise 2")
-print(is_vowel("A"))
-print(is_vowel("p"))
-print(is_vowel("1"))
 
 # 3. Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. 
 #     Use your is_vowel function to accomplish this.
@@ -48,12 +39,7 @@ def is_consonant(letter):
         return only_letters and not is_vowel(letter)
     else:
         return False
-print("Exercise 3")
-print(is_consonant("B"))
-print(is_consonant("E"))
-print(is_consonant("O"))
-print(is_consonant("2")) 
-print(is_consonant("+"))
+
 
 
 # 4. Define a function that accepts a string that is a word. The function should capitalize the first letter of the word 
@@ -66,9 +52,7 @@ def cap_cons_word(word):
     if is_consonant(let):
         word = word.capitalize()
     return word
-print("Exercise 4")    
-print(cap_cons_word("coffee"))
-print (cap_cons_word("another string"))
+
 
 # 5. Define a function named calculate_tip. 
 #    It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
@@ -79,8 +63,7 @@ def calculate_tip (tip, bill_total):
         return total_tip
     else:
         return " The tip must be  betwwen 0 and 1"
-print("exercise 5")
-print (calculate_tip(.20,50))
+
 
 # 6. Define a function named apply_discount. It should accept a original price, and a discount percentage, 
 #    and return the price after the discount is applied.
@@ -88,18 +71,16 @@ print (calculate_tip(.20,50))
 def apply_discount(price, disc):
     total = price - (price * disc)
     return total
-print("exercise 6")
-print (apply_discount(100, .20))    
+
 
 
 # 7. Define a function named handle_commas. It should accept a string that is a number that contains commas in it as input,
 #  and return a number as output.
 
-def remove_commas(string):
+def handle_commas(string):
     string = string.replace(",", "")
     return string
-print("exercise 7")
-print(remove_commas("30,456"))
+
 
 # 8. Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).
 def get_letter_grade(number):
@@ -113,8 +94,7 @@ def get_letter_grade(number):
         return "D"
     else:
         return "F"
-print("Exercise 8")
-print(get_letter_grade(48))   
+ 
 
 # 9. Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
 
@@ -125,8 +105,7 @@ def remove_vowels(string):
         if is_vowel(letter):
             new_word = new_word +letter
     return(new_word)
-print("Exercise 9")
-print(remove_vowels("Cindyaes"))
+
 
 # 10. Define a function named normalize_name. It should accept a string and return a valid python identifier, that is:
 #  anything that is not a valid python identifier should be removed
@@ -138,59 +117,27 @@ print(remove_vowels("Cindyaes"))
 #  First Name will become first_name
 #  % Completed will become completed
 
-# replace  " " for _ and making my string lowercase
-def repl_us(string):
-    """
-    this function replace all spaces with _ . lowercase the string
-    """
-    string = string.replace(" ", "_")
-    string = string.lower()
-    print(string)
-    return string
-
-
-def second_valid(string):
-    """
-    this function calculate  the position of the first letter of the string, so we can 
-    have a nre string strating from the first letter 
-    """
-    x=0
-    print(x)
-    if string[0].isdigit:
-        for letter in range (len(string)): 
-            #if string[letter].isdigit: 
-            if string[letter] in "0123456789":
-                x+= 1
-                if x!= len(string) and string[x].isalpha(): # we are checking if after a number is a letter.
-                    break
-    print(string, "valor x", x)        
-    if x == len(string):
-        string = " "
-    else:
-        string = string [x-1:] #x has the position of the first letter
-    return string
-
-def third_valid (string):
-    """
-    removes everything except numbers, leters and underscore"
-    """
-    new_string =" "
-    for letter in string:
-        if letter.isalnum() or letter == "_":
-            new_string = new_string +letter
-    return new_string
-
 
 def normalize_name(string):
-    res = third_valid(second_valid(repl_us(string)))
-    if res == " ":
-        print("Invalid python identifier")
+    """
+    Accept a string and return a valid python identifier
+    """
+    # remove any leading or trailing spaces replace space with _, and lowercase the string
+    string = string.strip().replace(" ", "_").lower()
+    #remove all the symbols
+    new_string = ""
+    for letter in string:
+        if letter.isalnum() or letter == "_":
+            new_string = new_string + letter
+    # remove all the numbers in the beggining of the string
+    for letter in range (len(new_string)):
+        if not new_string[0].isalpha():
+            new_string = new_string[1:]
+    if new_string == "":
+        return print(" You entered an invalid python identifier")
     else:
-        print("the valid python identifier is: ", res)
-        return res 
+        return new_string
 
-string =  input('Enter a string :  ')
-normalize_name(string)
 
 
 #11. Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
@@ -207,3 +154,25 @@ def cumulative_sum(my_list):
     return new_list
 
 
+# def normalize_name(string):
+#     output = ""
+    
+#     # lowercase all the things
+#     string = string.lower()
+
+#     # Filter out any non-valid identifiers, keep spaces to turn into _
+#     for character in string:
+#         if character.isidentifier() or character == " ":
+#             output += character
+    
+#     # remove any leading or trailing spaces
+#     output = output.strip()
+    
+#     # replace " " with "_"
+#     output = output.replace(" ", "_")
+    
+#     return output
+
+# assert normalize_name("First Name") == "first_name"
+# assert normalize_name("%99 Compl9eted2") == "completed"
+# assert normalize_name(" sum of squares ") == "sum_of_squares"
